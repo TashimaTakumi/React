70ae1c0d5f7401c188068333f5896bd05e459021
TashimaTakumi <b1015103@fun.ac.jp>
2017-07-19 11:55:46 +0900

    7/19

---
+++ reactbook-boiler/js/source/components/Logo.js
@@ -0,0 +1,9 @@
+//var React=require('react');
+import React from 'react';
+class Logo extends React.Component{
+	render(){
+		return <div className="Logo" />;
+	}
+}
+export default Logo
+//module.exports=Logo;
